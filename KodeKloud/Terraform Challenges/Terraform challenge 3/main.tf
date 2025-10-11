40cdd1bfecaeb896cb3898c04c6c48a420438ddf
Htun Htun Naing <htunhtun.naing@atom.com.mm>
2025-10-11 13:56:03 +0630

    final Answer

--- KodeKloud/Terraform Challenges/Terraform challenge 3/main.tf
+++ KodeKloud/Terraform Challenges/Terraform challenge 3/main.tf
@@ -3,36 +3,19 @@ resource "aws_key_pair" "citadel-key" {
   public_key = file("/root/terraform-challenges/project-citadel/.ssh/ec2-connect-key.pub")
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "citadel"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "citadel"
-  }
-}
-
-resource "aws_instance" "foo" {
+resource "aws_instance" "citadel" {
   ami           = var.ami # us-west-2
   instance_type = var.instance_type
-}
-
-resource "aws_eip_association" "eip_assoc" {
-  instance_id = aws_instance.foo.id
-  allocation_id = aws_eip.eip.id
+  key_name = aws_key_pair.citadel-key.key_name
+  user_data = file("/root/terraform-challenges/project-citadel/install-nginx.sh")
+  
 }
 
 resource "aws_eip" "eip" {
   vpc = true
-  instance = aws_instance.foo.id
-  
+  instance = aws_instance.citadel.id
+  provisioner "local-exec" {
+    command = "echo ${self.public_dns} >> /root/citadel_public_dns.txt"
+  }
+
 }
